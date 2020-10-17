a2f49f8b66f1618b742097758a914e51cbdf314c
wtong98 <williamtong105@gmail.com>
2020-10-17 12:49:32 -0500

    Add rudimentary server

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bayz: Live coding with Bayesian Program Learning
+Woot woot!
+
+### TODO
+* centralize constants in `common.py`
+* convert this TODO list to "issues"
---
+++ bayz/server.py
@@ -0,0 +1,52 @@
+"""
+Manages and launches Bayz server instance
+
+author: William Tong (wlt2115@columbia.edu)
+"""
+
+import http.server
+import json
+import threading
+
+class BayzServer:
+    def __init__(self, port):
+        self.port = port
+        self.httpd = None
+
+    def start(self):
+        def _startServer():
+            server_address = ('', self.port)
+            self.httpd = http.server.HTTPServer(server_address, BayzServer.BayzRequestHandler)
+            self.httpd.serve_forever()
+
+        if self.httpd is None:
+            process = threading.Thread(target=_startServer)
+            process.daemon = True
+            process.start()
+
+
+    class BayzRequestHandler(http.server.BaseHTTPRequestHandler):
+        def do_HEAD(self):
+            self._send_headers()
+
+        def do_GET(self):
+            self._send_headers()
+            json_data = json.dumps({'hello': 'world', 'received': 'ok'})
+            self.wfile.write(json_data.encode('utf-8'))
+
+        def _send_headers(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json; charset=utf-8')
+            self.end_headers()
+
+
+    def stop(self):
+        if self.httpd is not None:
+            self.httpd.shutdown()
+            self.httpd = None
+
+
+if __name__ == '__main__':
+    srv = BayzServer(42700)
+    srv.start()
+    input()
